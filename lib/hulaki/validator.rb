abf0174cfe5e1730ab86b362bdf4c13020231f8a
ccr <sis.ccr@gmail.com>
2016-02-20 15:40:09 +0545

    Validator class refracatored and test refractroed. test for validation of presence success

--- lib/hulaki/validator.rb
+++ lib/hulaki/validator.rb
@@ -1,21 +1,25 @@
 class Hulaki::EmailValidator
 
-  def initializer(email)
-    @email = email
+  def initialize(options = {} )
+    @sender_email = options[:from]
+    @reciepient_email = options[:to]
+    @message = options[:message]
+    @errors = {}
   end
 
-  def valid_email?
-    is_present?
-  end
-  def valid_message?
-    
+  def validates_presence
+    validate_sender_email && validate_recipient_email && validate_message
+    @errors
   end
   private
+    def validate_sender_email
+      @errors.store("sender_email", "sender_email must be present") if  @sender_email.nil?||@sender_email.empty?
+    end
+    def validate_recipient_email
+      @errors.store("reciepient_email", "reciepient_email must be present") if @reciepient_email.nil?||  @reciepient_email.empty?
+    end
+    def validate_message
+      @errors.store("message", "Message cannot be blank") if @message.nil? || @message.empty?
+    end
 
-  def is_present?
-    @email.nil?
-  end
-  def valid_format?
-
-  end
 end
--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -1,20 +1,21 @@
 require 'spec_helper'
-
 describe Hulaki::EmailValidator do
-  let(:validator) { Hulaki::EmailValidator.new }
+  let(:validator) { Hulaki::EmailValidator.new(:from=> "a", :to=> "b", :message=>"hello")  }
   context "Validates format of email" do
     it "should validate format of sender's email"
+
     it "should validate format of reciepient's email"
   end
+
   context "Validates presence of email" do
     it "should validate presence of sender's email" do
-      expect(validator.valid_email?).to eq(true)
+      expect(validator.validates_presence).not_to include("sender_email")
     end
     it "should validate presence of reciepient's email" do
-      expect(validator.valid_email?).to eq(true)
+      expect(validator.validates_presence).not_to include("reciepient_email")
     end
     it "should validate presence of message body" do
-      expect(validator.valid_message?).to eq(true)
+      expect(validator.validates_presence).not_to include("message")
     end
   end
 end
