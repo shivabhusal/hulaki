5e4ce4ed82f8b20f96ccd0603e8ea9c7547a8921
ccr <sis.ccr@gmail.com>
2016-02-20 13:45:29 +0545

    valid_email? and valid_message?  method added and two private methods added.

--- lib/hulaki/validator.rb
+++ lib/hulaki/validator.rb
@@ -1,2 +1,21 @@
-class Hulaki::Validator
+class Hulaki::EmailValidator
+
+  def initializer(email)
+    @email = email
+  end
+
+  def valid_email?
+    is_present?
+  end
+  def valid_message?
+    
+  end
+  private
+
+  def is_present?
+    @email.nil?
+  end
+  def valid_format?
+
+  end
 end
--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -1,15 +1,20 @@
 require 'spec_helper'
-  before :each do
-    
-  end
-describe Hulaki::Validator do
+
+describe Hulaki::EmailValidator do
+  let(:validator) { Hulaki::EmailValidator.new }
   context "Validates format of email" do
     it "should validate format of sender's email"
     it "should validate format of reciepient's email"
   end
   context "Validates presence of email" do
-    it "should validate presence of sender's email"
-    it "should validate presence of reciepient's email"
-    it "should validate presence of message body"
+    it "should validate presence of sender's email" do
+      expect(validator.valid_email?).to eq(true)
+    end
+    it "should validate presence of reciepient's email" do
+      expect(validator.valid_email?).to eq(true)
+    end
+    it "should validate presence of message body" do
+      expect(validator.valid_message?).to eq(true)
+    end
   end
 end
