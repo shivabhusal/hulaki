f70a7f01efd06c2d710052846b76eac30a6cb051
john <hotline.shiva@gmail.com>
2016-02-20 17:30:39 +0545

    Spec added : it 'should return say that the sender phone-number is required'

--- lib/hulaki/sms_handler/sms_validator.rb
+++ lib/hulaki/sms_handler/sms_validator.rb
@@ -1,15 +1,17 @@
 class Hulaki::SmsValidator
-  attr_reader :errors, :to, :message, :gateway
+  attr_reader :errors, :to, :from, :message, :gateway
   RegexPhoneNumber = /(?:\+?|\b)[0-9]{10}\b/
 
   def initialize(params = {})
     @errors = {}
     @to = params.fetch(:to, params[:recipient])
+    @from = params.fetch(:from, params[:sender])
     @message = params.fetch(:message, params[:msg])
   end
 
   def validate
-    to || (raise Hulaki::InvalidPhoneNumber, 'Phone number should be present.')
+    to || (raise Hulaki::InvalidPhoneNumber, 'Recipient phone number should be present.')
+    from || (raise Hulaki::InvalidPhoneNumber, 'Sender phone number should be present.')
     message || (raise Hulaki::InvalidMessageBody, 'Message body should be present.')
 
     check_if_number_invalid
@@ -18,6 +20,9 @@ class Hulaki::SmsValidator
   private
   def check_if_number_invalid
     RegexPhoneNumber.match(to) || (raise Hulaki::InvalidPhoneNumber,
-                                         'Phone number should be valid.')
+                                         "Phone number '#{to}' is invalid.")
+
+    RegexPhoneNumber.match(from) || (raise Hulaki::InvalidPhoneNumber,
+                                           "Phone number '#{from}' is invalid.")
   end
 end
--- spec/sms_validator_spec.rb
+++ spec/sms_validator_spec.rb
@@ -5,20 +5,29 @@ describe Hulaki::SmsValidator, type: :policy do
     let(:valid_number) { '9843488767' }
     let(:invalid_number) { '1asd1111111' }
     context 'Presence Validation' do
-      it 'should return say that the phone-number is required' do
-        expect { Hulaki::SmsValidator.new(msg: 'hello').validate }.to \
+      it 'should return say that the recipient phone-number is required' do
+        expect { Hulaki::SmsValidator.new(msg: 'hello', to: valid_number).validate }.to \
+        raise_error(Hulaki::InvalidPhoneNumber)
+      end
+
+      it 'should return say that the sender phone-number is required' do
+        expect { Hulaki::SmsValidator.new(msg: 'hello',
+                                          from: valid_number).validate }.to \
         raise_error(Hulaki::InvalidPhoneNumber)
       end
 
       it 'should return say that the message is required' do
-        expect { Hulaki::SmsValidator.new(to: valid_number).validate }.to \
+        expect { Hulaki::SmsValidator.new(to: valid_number,
+                                          from: valid_number).validate }.to \
         raise_error(Hulaki::InvalidMessageBody)
       end
     end
 
     context 'Validation of Phone Number' do
       it 'should raise exception when number is invalid' do
-        expect { Hulaki::SmsValidator.new(msg: 'hello', to: invalid_number).validate }.to \
+        expect { Hulaki::SmsValidator.new(msg: 'hello',
+                                          to: invalid_number,
+                                          from: valid_number).validate }.to \
         raise_error(Hulaki::InvalidPhoneNumber)
       end
     end
