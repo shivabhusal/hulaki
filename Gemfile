0ac1ccd36db7f443bb829b0fca248ebe9cb59da5
ccr <sis.ccr@gmail.com>
2016-02-20 18:08:11 +0545

    gem mail added

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in hulaki.gemspec
 gemspec
 gem 'pry'
+gem 'mail'
--- lib/hulaki.rb
+++ lib/hulaki.rb
@@ -1,15 +1,13 @@
 require "hulaki/version"
-<<<<<<< HEAD
-require 'hulaki/validator'
-require 'hulaki/mailer'
-
-module Hulaki
-  # Your code goes here...
-end
-
 require "hulaki/sms_handler/sms_handler"
 require "hulaki/sms_handler/sms_validator"
 require "hulaki/sms_handler/gateway_adapters/twilio"
 require "hulaki/sms_handler/gateway_adapters/sparrow"
 require 'hulaki/config'
 require 'hulaki/exceptions'
+require 'hulaki/validator'
+require 'hulaki/mailer'
+
+module Hulaki
+  # Your code goes here...
+end
