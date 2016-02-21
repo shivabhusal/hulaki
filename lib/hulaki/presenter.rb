01ddb88ebb5ebc558c0e51fe9893fd5b99adbd7b
john <hotline.shiva@gmail.com>
2016-02-21 14:55:07 +0545

    Refactored

--- lib/hulaki/presenter.rb
+++ lib/hulaki/presenter.rb
@@ -4,10 +4,14 @@ class Hulaki::Presenter
   end
 
   def display
+    puts '~' * 100
+    puts 'Search Results'
+    puts '~' * 100
     @data[0..5].each do |row|
       email = row.fetch('e_mail_1___value', 'N/A') rescue 'N/A'
       phone_number = row.fetch('phone_1___value', 'N/A') rescue 'N/A'
       puts "name: #{row['name']}, phone: #{phone_number}, email: #{email}" rescue nil
     end
+    puts '~' * 100
   end
 end
