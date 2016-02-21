30ebc4eed53140e89b594479dbbf4196858b960a
john <hotline.shiva@gmail.com>
2016-02-21 13:19:13 +0545

    Refactored to be usable from any path in system

--- lib/hulaki/option_parser.rb
+++ lib/hulaki/option_parser.rb
@@ -1,7 +1,8 @@
 require_relative 'recursive_ostruct'
 require 'fileutils'
-class Hulaki::OptionParser
+require 'optparse'
 
+class Hulaki::OptionParser
   def initialize
     @config = RecursiveOstruct.ostruct(
         {
@@ -14,7 +15,6 @@ class Hulaki::OptionParser
 
   def parse(args)
     options.parse!(args)
-
     @config
   end
 
@@ -81,25 +81,31 @@ class Hulaki::OptionParser
   end
 
   def start_copying_file
-    puts 'Creating ~/hulaki/config.yml ...'
-    file_path = File.expand_path('lib/hulaki/config/config_sample.yml')
+    this_file = __FILE__
+    file_path = File.expand_path('../../lib/hulaki/config/config_sample.yml',
+                                 File.dirname(this_file))
     desc_file = File.expand_path('~/hulaki/config.yml')
     if File.exists?(desc_file)
-      puts "Looks like the file #{desc_file} already exists."
+      puts "Looks like the file '#{desc_file}' already exists."
       puts 'shall we forcefully override the file?(yes/no)'
-      input = gets().chomp()
-      if ['yes', 'y'].include?(input.downcase)
-        copy_file(file_path, desc_file)
-      else
-        puts 'You choose to leave it as it is.'
-      end
+      handle_conflict(file_path, desc_file)
     else
       copy_file(file_path, desc_file)
     end
   end
 
+  def handle_conflict(file_path, desc_file)
+    input = gets().chomp()
+    if %w{yes y}.include?(input.downcase)
+      copy_file(file_path, desc_file)
+    else
+      puts 'You choose to leave it as it is.'
+    end
+  end
+
   def copy_file(file_path, desc_file)
+    puts "Creating file '~/hulaki/config.yml' ..."
     FileUtils.cp(file_path, desc_file)
-    puts 'File copied.'
+    puts "Created file '~/hulaki/config.yml' ..."
   end
 end
