4f83e0e17384ef19dd0ed0a8dc7ee273e8003632
john <hotline.shiva@gmail.com>
2016-02-21 14:32:19 +0545

    Integrated the Hulaki::Core with contact parser and search engine

--- lib/hulaki/search_engine.rb
+++ lib/hulaki/search_engine.rb
@@ -1,39 +1,38 @@
-include Amatch
 class Hulaki::SearchEngine
-
+  include Amatch
   def initialize
     contact_parser = Hulaki::ContactParser.new
-    @csv_data = contact_parser.process
+    @csv_data = contact_parser.perform
   end
 
-  def perform_action(search_data)
+  def perform(search_data)
     search(search_data)
   end
 
   private
 
   def search(data)
-    keys_to_search = ["name", "email", "phone"]
-
-    search_result = @csv_data.map do |row|
-                        pt = 0.0
-                        row.each do |k,v|
-                          keys_to_search.each do |key|
-                            if k.to_s.match(key.to_s)
-                              pt+=PairDistance.new(v.to_s).match(data.to_s)
-                            end
-                          end
-                        end
-                        weight = pt/keys_to_search.count
-                        percentage = weight *100
-
-
-                        [row, percentage]
-                    end
-
-    result = search_result.select{|x| x[1] > 0.3}
-    result.sort {|x,y| y[1] <=> x[1]}
-
+    result = search_result(data).select { |x| x[1] > 0.3 }
+    result.sort { |x, y| y[1] <=> x[1] }
   end
-  
+
+  def search_result(data)
+    @csv_data.map do |row|
+      pt = 0.0
+      row.each do |k, v|
+        keys_to_search.each do |key|
+          if k.to_s.match(key.to_s)
+            pt += PairDistance.new(v.to_s).match(data.to_s)
+          end
+        end
+      end
+      weight = pt / keys_to_search.count
+      percentage = weight *100
+      [row, percentage]
+    end
+  end
+
+  def keys_to_search
+    ['name', 'email', 'phone']
+  end
 end
